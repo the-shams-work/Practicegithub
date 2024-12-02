1738ca8d64c111bb59602f712a7287fc6ba4bc60
Shams Tabrej Alam <shams.22scse1010782@galgotiasuniversity.edu.in>
2024-12-02 15:27:55 +0530

    Initial Commit

---
+++ PracticegithubTests/PracticegithubTests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticegithubTests.swift
+//  PracticegithubTests
+//
+//  Created by Shams Tabrej Alam on 02/12/24.
+//
+
+import Testing
+@testable import Practicegithub
+
+struct PracticegithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
